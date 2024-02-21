4c6918d7c573852474a0f8e94ea5539880882ec2
Oliver Eddy <olivereddy.developer@gmail.com>
2024-02-21 16:20:11 +1300

    fixed a href button

--- src/components/layouts/HeaderButton.js
+++ src/components/layouts/HeaderButton.js
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const HeaderButton = () => {
-
-    return (
-        <>
-            <div className="mx-auto">
-            <button className="see">
-  <span className="text-icon-wrapper">Checkout Portfolio<i className="fas fa-hand-point-down"></i></span>
-</button>
-
-            </div>
-        </>
-    )
+  return (
+    <div className="mx-auto">
+      {/* Wrap the button in an anchor tag for navigation */}
+      <a href="#about" className="see-anchor">
+        <button className="see">
+          <span className="text-icon-wrapper">Checkout Portfolio<i className="fas fa-hand-point-down"></i></span>
+        </button>
+      </a>
+    </div>
+  );
 }
 
-export default HeaderButton
+export default HeaderButton;
