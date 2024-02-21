b503ad3c8fa73701c501880669197cd08c694e6c
Oliver Eddy <olivereddy.developer@gmail.com>
2024-02-22 11:55:59 +1300

    Add smooth scrolling functionality to HeaderButton.js and update button styling in no-touch.min.css

--- src/components/layouts/HeaderButton.js
+++ src/components/layouts/HeaderButton.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const HeaderButton = () => {
+  const handleClick = (event) => {
+    event.preventDefault();
+    const aboutSection = document.getElementById('about');
+    const style = window.getComputedStyle(aboutSection);
+    const marginTop = parseInt(style.marginTop.replace('px', ''));
+    const topPosition = aboutSection.offsetTop - marginTop;
+    window.scrollTo({ top: topPosition, behavior: 'smooth' });
+  };
+
   return (
     <div className="mx-auto">
-      {/* Wrap the button in an anchor tag for navigation */}
-      <a href="#about" className="see-anchor">
+      <a href="#about" className="see-anchor" onClick={handleClick}>
         <button className="see">
           <span className="text-icon-wrapper">Checkout Portfolio<i className="fas fa-hand-point-down"></i></span>
         </button>
