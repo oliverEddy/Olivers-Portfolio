281cc36f3c1f99288f13097cdb367a3fecfde3d4
Oliver Eddy <olivereddy.developer@gmail.com>
2024-02-25 12:11:35 +1300

    Refactor About component layout using CSS grid

--- src/components/section/About.js
+++ src/components/section/About.js
@@ -2,32 +2,28 @@ import React from 'react'
 import Skills from '../layouts/Skills'
 import { about, skillsBar, section2title } from '../../profile'
 import './About.css'
+
 const About = () => {
-    return (
-        <div id="about" className="effect2">
-            {/* New wrapper div for the top shadow effect */}
-            <div className="effect2-top-wrapper">
-                <div data-aos="zoom-in-up" data-aos-once="true" className="row">
-                    <div className="col-12 d-none d-md-block offset-md-1 col-md-4 Photo" id="not-dark2"></div>
-                    <div className="col-12 offset-md-1 col-md-6 about">
-                        <div className="About-title-box">
-                            <h1 id="About" className="red-line">{section2title}</h1>
-                        </div>
-                        <p className="lead about-text">
-                            {about.paragraph}
-                        </p>
-                    </div>
-                </div>
-                <div id="Skills"> 
-                    <div className="row d-flex justify-content-center skills">
-                        {skillsBar.map((x) => 
-                            <Skills key={x.name} faClass={x.faClass} svg={x.svg} label={x.name}/>
-                        )}
-                    </div> 
-                </div>
-            </div>
-        </div>
-    )
+   return (
+       <div id="about" className="effect2">
+           <div className="grid-container"> {/* Updated wrapper for grid layout */}
+               <div className="grid-item Photo" id="not-dark2"></div> {/* Photo as grid item */}
+               <div className="grid-item about"> {/* About text as grid item */}
+                   <div className="About-title-box">
+                       <h1 id="About" className="red-line">{section2title}</h1>
+                   </div>
+                   <p className="lead about-text">
+                       {about.paragraph}
+                   </p>
+               </div>
+               <div className="grid-item skills" id="Skills"> {/* Skills as grid item */}
+                   {skillsBar.map((x) =>
+                       <Skills key={x.name} faClass={x.faClass} svg={x.svg} label={x.name}/>
+                   )}
+               </div>
+           </div>
+       </div>
+   )
 }
 
 export default About
