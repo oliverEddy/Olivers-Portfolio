583b1aca1209fc2e39cd35ddd0baf419dd13ac81
Oliver Eddy <olivereddy.developer@gmail.com>
2024-03-04 19:13:46 +1300

    Put all font awesome Icons in the same row, on the contact page form

--- src/components/section/Contact.js
+++ src/components/section/Contact.js
@@ -52,15 +52,25 @@ const Contact = () => {
                 ))}
               </p>
               <div className="d-flex justify-content-center align-items-center flex-column">
-                <div className="inline-block">
-                  {social.linkedin && <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank"  href={social.linkedin}><i className="fab fa-linkedin"></i></a>}
-                    {social.facebook && <a title="Visit Facebok profile" rel="noopener noreferrer" target="_blank" href={social.facebook}><i className="fab fa-facebook"></i></a>}
-                    {social.twitter && <a title="Visit Twitter profile" rel="noopener noreferrer" target="_blank" href={social.twitter}><i className="fab fa-twitter"></i></a>}
-                    {social.instagram && <a title="Visit Instagram profile" rel="noopener noreferrer" target="_blank" href={social.instagram}><i className="fab fa-instagram"></i></a>}
-                    {social.github && <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}><i className="fab fa-github"></i></a>}<br/>
-                </div>
-                {social.resume && <a title="Download Resume" href={social.resume} download><i className="fas fa-download"></i></a>}
-              </div>
+    <div className="inline-block">
+        {social.linkedin && (
+            <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank" href={social.linkedin}>
+                <i className="fab fa-linkedin"></i>
+            </a>
+        )}
+        {social.github && (
+            <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}>
+                <i className="fab fa-github"></i>
+            </a>
+        )}
+        {social.resume && (
+            <a title="Download Resume" href={social.resume} download>
+                <i className="fas fa-download"></i>
+            </a>
+        )}
+    </div>
+</div>
+
             </div>
           </div>
         </div>
