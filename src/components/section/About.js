d09a827dd30ec0183d5339b299b11ae6a4af15e9
Oliver Eddy <olivereddy.developer@gmail.com>
2024-03-08 15:47:12 +1300

    Update About section styling and content

--- src/components/section/About.js
+++ src/components/section/About.js
@@ -1,35 +1,9 @@
-import React, { useEffect } from 'react';
 import Skills from '../layouts/Skills';
 import { about, skillsBar, section2title } from '../../profile';
 import './About.css';
 
 const About = () => {
-  // Resize text based on container's width
-  const adjustTextSize = () => {
-    const container = document.querySelector('.grid-item.about'); // Container
-    if (!container) return; // Guard clause if container is not found
-
-    const textElements = container.querySelectorAll('p, h1, .lead'); // Text elements to resize
-    let containerWidth = container.offsetWidth; // Get the container's width
-    let scaleFactor = 0.035; // Adjust this scale factor as needed
-
-    textElements.forEach(element => {
-        let newSize = containerWidth * scaleFactor; // Initial font size calculation
-        newSize = Math.max(newSize, 10); // Allow smaller sizes if necessary
-        newSize = Math.min(newSize, 18); // Set a maximum size if needed
-        element.style.fontSize = `${newSize}px`;
-      });
-      
-  };
-
-  useEffect(() => {
-    adjustTextSize(); // Adjust text size on mount
-    const handleResize = () => adjustTextSize(); // Create a resize event handler
-
-    window.addEventListener('resize', handleResize); // Add resize event listener
-
-    return () => window.removeEventListener('resize', handleResize); // Cleanup
-  }, []);
+ 
 
   return (
     <div id="about" className="effect2">
@@ -39,7 +13,7 @@ const About = () => {
           <div className="About-title-box">
             <h1 id="About" className="red-line">{section2title}</h1>
           </div>
-          <p className="lead about-text">
+          <p className="lead about-text" >
             {about.paragraph}
           </p>
         </div>
