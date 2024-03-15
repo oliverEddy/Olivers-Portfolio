ade4ca830855b975bb264f3cd3bff8f3703f512e
Oliver Eddy <olivereddy.developer@gmail.com>
2024-03-15 15:33:54 +1300

    Connect for to form spree

--- src/components/section/Contact.js
+++ src/components/section/Contact.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { contact, section5Title, social } from '../../profile';
-import './Contact.css';
+import React from "react";
+import { contact, section5Title, social } from "../../profile";
+import "./Contact.css";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function useWindowSize() {
   const [size, setSize] = useState([window.innerWidth, 0]);
@@ -11,13 +11,14 @@ function useWindowSize() {
     function updateSize() {
       setSize([window.innerWidth, window.innerHeight]);
     }
-    window.addEventListener('resize', updateSize);
+    window.addEventListener("resize", updateSize);
     updateSize();
-    return () => window.removeEventListener('resize', updateSize);
+    return () => window.removeEventListener("resize", updateSize);
   }, []);
 
   return size;
 }
+
 
 const Contact = () => {
   const [width] = useWindowSize(); // Using the custom hook to get current window width
@@ -26,49 +27,91 @@ const Contact = () => {
     <div className="parallax">
       <div data-aos="zoom-in-up" data-aos-once="true" className="git-form">
         <div className="git-head-div text-center mx-auto">
-          <h1 id="Contact" className="git-head">{section5Title}</h1>
+          <h1 id="Contact" className="git-head">
+            {section5Title}
+          </h1>
         </div>
         <div className="container">
           <div className="git-cont row">
             <div className="col-12 col-sm-6 half">
-              <form action={contact.contactUrl ? contact.contactUrl : "https://formspree.io"} method={contact.contactUrl ? "POST" : "GET"}>
-                <input type="text" id="fname" name="firstname" placeholder="Your name" required></input>
-                <input type="mail" id="mailid" name="Email" placeholder="Email Address" required></input>
-                <input type="text" id="sub" name="Subject" placeholder="Subject" required></input>
-                <textarea id="msg" name="message" placeholder="Message" required style={{ resize: 'none' }}></textarea>
-                <button style={{ cursor: 'pointer' }} type="submit">Send Message</button>
+              <form action="https://formspree.io/f/xqkrlrqv" method="POST">
+                <input
+                  type="text"
+                  id="fname"
+                  name="firstname"
+                  placeholder="Your name"
+                  required
+                ></input>
+                <input
+                  type="mail"
+                  id="mailid"
+                  name="Email"
+                  placeholder="Email Address"
+                  required
+                ></input>
+                <input
+                  type="text"
+                  id="sub"
+                  name="Subject"
+                  placeholder="Subject"
+                  required
+                ></input>
+                <textarea
+                  id="msg"
+                  name="message"
+                  placeholder="Message"
+                  required
+                  style={{ resize: "none" }}
+                ></textarea>
+                <button style={{ cursor: "pointer" }} type="submit">
+                  Send Message
+                </button>
               </form>
             </div>
             <div className="col-12 col-sm-6 half">
-            <p className="lead">
-  {contact.pitch.map((line, index) => (
-    <React.Fragment key={index}>
-      {line}
-      {index < contact.pitch.length - 1 && <><br /><br /></>}
-    </React.Fragment>
-  ))}
-</p>
+              <p className="lead">
+                {contact.pitch.map((line, index) => (
+                  <React.Fragment key={index}>
+                    {line}
+                    {index < contact.pitch.length - 1 && (
+                      <>
+                        <br />
+                        <br />
+                      </>
+                    )}
+                  </React.Fragment>
+                ))}
+              </p>
 
               <div className="d-flex justify-content-center align-items-center flex-column">
-    <div className="inline-block">
-        {social.linkedin && (
-            <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank" href={social.linkedin}>
-                <i className="fab fa-linkedin"></i>
-            </a>
-        )}
-        {social.github && (
-            <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}>
-                <i className="fab fa-github"></i>
-            </a>
-        )}
-        {social.resume && (
-            <a title="Download Resume" href={social.resume} download>
-                <i className="fas fa-download"></i>
-            </a>
-        )}
-    </div>
-</div>
-
+                <div className="inline-block">
+                  {social.linkedin && (
+                    <a
+                      title="Visit Linkedin profile"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      href={social.linkedin}
+                    >
+                      <i className="fab fa-linkedin"></i>
+                    </a>
+                  )}
+                  {social.github && (
+                    <a
+                      title="Visit Github profile"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      href={social.github}
+                    >
+                      <i className="fab fa-github"></i>
+                    </a>
+                  )}
+                  {social.resume && (
+                    <a title="Download Resume" href={social.resume} download>
+                      <i className="fas fa-download"></i>
+                    </a>
+                  )}
+                </div>
+              </div>
             </div>
           </div>
         </div>
